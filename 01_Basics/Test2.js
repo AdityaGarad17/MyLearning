98cb4d3e473ab115694bee5f3024116d1993b69b
AdityaGarad17 <112620791+AdityaGarad17@users.noreply.github.com>
2025-04-28 17:13:05 +0000

    datatypes

---
+++ 01_Basics/Test2.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3); console will give 6 as output, here we are using node.js not browser
+// maintain code readability. Extension prettier use to indent code 
+
+
+/*
+datatypes in JS :- 
+
+number => 2 power 53
+bigint 
+string 
+boolean
+null :-  standlone value in JS (sperate type)
+undefined :- value is not assigned ( declared but not initalized )
+symbol :- unique 
+
+object
+
+Undefined is type itself
+null is type object
+*/
+ let Val = true;
+
+ let val = "Aditya"
+ console.log(typeof val)
